fdb79ec5029473b085121a20f0aec3fc19854427
NAZHIFAH FAIRUZ <nazhifahfairuz@apps.ipb.ac.id>
2025-11-23 15:32:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5952223023-c6363",
+  "appId": "1:667051110772:web:7d2d8abadbd8e30de33c21",
+  "apiKey": "AIzaSyCgql2xSwOUl5FYZAN8oj-Uct178CMpziU",
+  "authDomain": "studio-5952223023-c6363.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "667051110772"
+};
